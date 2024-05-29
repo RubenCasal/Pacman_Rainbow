2865bfecafd5551b7d0056f2b2357b0bd6fff502
RubenCasal <72756524+RubenCasal@users.noreply.github.com>
2024-05-29 21:32:26 +0200

    comentarios tutorial

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from environment import Environment
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-MODEL_PATH = './experimentation_models/pacmanTorch.pth'
-SAVE_MODEL_PATH = './experimentation_models/pacmanTorch.pth'
-MODE = 'test'
+#Modelos para probar de las distintas estrategias
+#pacmanTorch_all_same_reward.pth | Todas las acciones tienen la misma recompensa
+#pacmanTorch_no_rewards_eat_ghosts.pth | No obtiene recompensa por comer fantasmas
+#pacmanTorch_standar_rewards.pth | Con las recompensas que ofrece inicialmente el entorno de gymnasium
+#pacmanTorch_rewards_keep_living.pth | Obtiene recompensas periodicamente por sobrevivir cuando alcanza un número determinado de pasos
+#pacmanTorch_best_model_log.pth | Mejor modelo entrenado con recompensas normalizadas
+
+
+MODEL_PATH = './experimentation_models/pacmanTorch_best_model_log.pth' # Aquí modelo que se quiera probar para el testing
+SAVE_MODEL_PATH = './experimentation_models/pacmanTorch.pth' #Ruta donde se va a guardar el modelo
+MODE = 'test' # test | train
 GRAPH_PATH = './experimentation/pacman.png'
 EPISODES_TRAIN = 12001
 LEARNING_RATE = 0.0001
